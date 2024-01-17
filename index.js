649350084e049928c51b879be070c150dc1ea744
Ndzhaka Mabasa <146018535+Ndzhaka@users.noreply.github.com>
2024-01-17 11:09:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function add1(){
+let score = document.getElementById("home-score");
+    score.textContent = parseInt(score.textContent) + 1;}
+
+    function add2(){
+        let score = document.getElementById("home-score");
+            score.textContent = parseInt(score.textContent) + 2;}
+
+            function add3(){
+                let score = document.getElementById("home-score");
+                    score.textContent = parseInt(score.textContent) + 3;}
+
+                    function add12(){
+                        let score = document.getElementById("home-score2");
+                            score.textContent = parseInt(score.textContent) + 1;}
+                        
+                            function add22(){
+                                let score = document.getElementById("home-score2");
+                                    score.textContent = parseInt(score.textContent) + 2;}
+                        
+                                    function add33(){
+                                        let score = document.getElementById("home-score2");
+                                            score.textContent = parseInt(score.textContent) + 3;}
